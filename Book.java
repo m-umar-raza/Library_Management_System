ba518d95f03599aaf40bf692af8ec82b3a963eac
m-umar-raza <154897399+m-umar-raza@users.noreply.github.com>
2024-03-14 23:28:13 +0500

    Book.java

--- Book.java
+++ Book.java
@@ -1,209 +1,153 @@
 package Library_Management;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.*;
 
-public class Book {
-    // Attributes of a book
-    public int bookID;
-    public String title;
-    public String author;
-    public String genre;
-    public Boolean availabilityStatus;
+public class Library implements Serializable {
+    // ArrayLists to store books and users
+    public static ArrayList<Book> books = new ArrayList<>();
+    public static ArrayList<User> users = new ArrayList<>();
 
-    // Constructor to initialize book attributes
-    Book(int bookID, String title, String author, String genre, Boolean availabilityStatus) {
-        this.setBookID(bookID);
-        this.setTitle(title);
-        this.setAuthor(author);
-        this.setGenre(genre);
-        this.setAvailabilityStatus(availabilityStatus);
-    }
+    public static void main(String[] args) {
 
-    // Scanner object for user input
-    static Scanner in = new Scanner(System.in);
-
-    // Setter methods for book attributes
-    public void setBookID(int x) {
-        this.bookID = x;
-    }
-
-    public void setTitle(String t) {
-        this.title = t;
-    }
-
-    public void setAuthor(String a) {
-        this.author = a;
-    }
-
-    public void setGenre(String g) {
-        this.genre = g;
-    }
-
-    public void setAvailabilityStatus(Boolean as) {
-        this.availabilityStatus = as;
-    }
-
-    // Getter methods for book attributes
-    public int getBookID() {
-        return bookID;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public String getAvailabilityStatus() {
-        return (availabilityStatus ? "Available" : "Not Available");
-    }
-
-    // Method to add a new book
-    public static void addBook() {
-        System.out.println("Enter Book ID:");
-        int bookID;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Welcome to the Library Management System!");
+        System.out.println("Enter Username: ");
+        String userName = in.nextLine();
+        System.out.println("Enter Password");
+        int userPass;
         try {
-            bookID = Integer.parseInt(in.nextLine());
+            userPass = Integer.parseInt(in.nextLine());
         } catch (NumberFormatException e) {
-            System.out.println("Invalid input. Please enter a number for the book ID.");
+            System.out.println("Invalid input. Please enter a number for the password.");
             return;
         }
-        for (Book book : Library.books) {
-            if (book.getBookID() == bookID) {
-                System.out.println("Book is already present");
-                return;
-            }
-        }
-        System.out.println("Enter Title:");
-        String title = in.nextLine();
-        System.out.println("Enter Author:");
-        String author = in.nextLine();
-        System.out.println("Enter Genre:");
-        String genre = in.nextLine();
-        Library.books.add(new Book(bookID, title, author, genre, true));
-        System.out.println("*Book added Successfully*\n");
-    }
+        int myPass = 123;
+        String myUser = "Umar";
 
-    // Method to borrow a book
-    public static void borrowBook() {
-        System.out.println("Enter the book to be borrowed");
-        String bBook = in.nextLine();
-        System.out.println("Enter your User ID");
-        int uID;
-        try {
-            uID = Integer.parseInt(in.nextLine());
-        } catch (NumberFormatException e) {
-            System.out.println("Invalid input. Please enter a number for the User ID.");
-            return;
-        }
-        boolean userExists = false;
-        for (User user : Library.users) {
-            if (user.getUserID() == uID) {
-                userExists = true;
-                for (Book book : Library.books) {
-                    if (book.getTitle().equals(bBook)) {
-                        if (book.availabilityStatus) {
-                            user.borrowBooks(bBook);
-                            book.availabilityStatus = false; // Update availability status
-                            System.out.println("The book has been borrowed.");
-                        } else {
-                            System.out.println("The book is not available.");
-                        }
-                        break;
-                    }
-                }
-                System.out.println("The book is not available.");
+        // Authentication loop
+        while (true) {
+            if (userName.equals(myUser) && userPass == myPass) {
+                System.out.println("Authentication successful!");
+                loadData(); // Load data when authentication is successful
                 break;
-            }
-        }
-        if (!userExists) {
-            System.out.println("User ID does not exist.");
-        }
-    }
-
-    // Method to return a borrowed book
-    public static void returnBook() {
-        System.out.println("Enter the book to be Returned");
-        String rBook = in.nextLine();
-        System.out.println("Enter your User ID");
-        int uID;
-        try {
-            uID = Integer.parseInt(in.nextLine());
-        } catch (NumberFormatException e) {
-            System.out.println("Invalid input. Please enter a number for the User ID.");
-            return;
-        }
-        boolean userExists = false;
-        for (User user : Library.users) {
-            if (user.getUserID() == uID) {
-                userExists = true;
-                for (Book book : Library.books) {
-                    if (book.getTitle().equals(rBook)) {
-                        user.returnBooks(rBook);
-                        book.availabilityStatus = true; // Update availability status
-                        System.out.println("The book has been returned.");
-                        break;
-                    }
+            } else {
+                System.out.println("Wrong Username or Password");
+                System.out.println("Enter Username: ");
+                userName = in.nextLine();
+                System.out.println("Enter Password");
+                try {
+                    userPass = Integer.parseInt(in.nextLine());
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid input. Please enter a number for the password.");
+                    return;
                 }
-                break;
             }
         }
-        if (!userExists) {
-            System.out.println("User ID does not exist.");
-        }
-    }
 
-    // Method to search for a book by title or author
-    public static void searchBookByTitleOrAuthor() {
-        System.out.println("Enter a title or author to search for books: ");
-        String query = in.nextLine();
-        boolean bookFound = false;
-        for (Book book : Library.books) {
-            if (book.getTitle().equalsIgnoreCase(query) || book.getAuthor().equalsIgnoreCase(query)) {
-                bookFound = true;
-                System.out.println(book);
+        // Menu loop
+        int opt;
+        String again = "y";
+        while (again.equalsIgnoreCase("y")) {
+            System.out.println("""
+                    Please select an option:\s
+                    1. Add a User
+                    2. Add a Book
+                    3. Borrow a Book
+                    4. Return a Book
+                    5. Search for Books by User ID
+                    6. Display All Books
+                    7. Display All Users
+                    8. Search book by Title or Author
+                    9. Exit
+
+                    Enter your choice:\s""");
+            try {
+                opt = Integer.parseInt(in.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a number for the option.");
+                continue;
             }
-        }
-        if (!bookFound) {
-            System.out.println("No books found with the given title or author.");
+            switch (opt) {
+                case 1:
+                    User.addUser(); // Add a user
+                    saveData(); // Save data after adding user
+                    break;
+                case 2:
+                    Book.addBook(); // Add a book
+                    saveData(); // Save data after adding book
+                    break;
+                case 3:
+                    Book.displayTitles(); // Display book titles
+                    Book.borrowBook(); // Borrow a book
+                    saveData(); // Save data after borrowing book
+                    break;
+                case 4:
+                    Book.returnBook(); // Return a book
+                    saveData(); // Save data after returning book
+                    break;
+                case 5:
+                    User.searchBooksByUserID(); // Search for books by user ID
+                    break;
+                case 6:
+                    Book.displayBook(); // Display all books
+                    break;
+                case 7:
+                    User.displayUsers(); // Display all users
+                    break;
+                case 8:
+                    Book.searchBookByTitleOrAuthor(); // Search book by title or author
+                    break;
+                default:
+                    System.out.println("Invalid option! Please enter a valid option.");
+                    break;
+                case 9:
+                    System.out.println("Exiting...");
+                    System.exit(0); // Exit the program
+                    break;
+            }
+            System.out.println("Do you want to continue(y/n)? ");
+            again = in.nextLine();
         }
     }
 
-    // Method to display all books in the library
-    public static void displayBook() {
-        System.out.println("List of Books:");
-        System.out.println("---------------");
-        for (Book book : Library.books) {
-            System.out.println("Book ID: " + book.getBookID());
-            System.out.println("Title: " + book.getTitle());
-            System.out.println("Author: " + book.getAuthor());
-            System.out.println("Genre: " + book.getGenre());
-            System.out.println("Availability: " + (book.getAvailabilityStatus()));
-            System.out.println("---------------");
+    // Method to load data from file
+    public static void loadData() {
+        try {
+            FileInputStream fis = new FileInputStream("libraryData.txt");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            books = (ArrayList<Book>) ois.readObject();
+            users = (ArrayList<User>) ois.readObject();
+
+            ois.close();
+            fis.close();
+        } catch (FileNotFoundException e) {
+            // File does not exist, initialize books and users as empty lists
+            books = new ArrayList<>();
+            users = new ArrayList<>();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } catch (ClassNotFoundException c) {
+            System.out.println("Class not found");
+            c.printStackTrace();
         }
     }
 
-    // Method to display titles of all books in the library
-    public static void displayTitles() {
-        System.out.println("List of Titles:");
-        System.out.println("---------------");
-        int c = 1;
-        for (Book book : Library.books) {
-            System.out.println(c + ". " + book.getTitle());
-            c++;
-        }
-        System.out.println("---------------");
-    }
+    // Method to save data to file
+    public static void saveData() {
+        try {
+            FileOutputStream fos = new FileOutputStream("libraryData.txt");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
 
-    // Overriding toString method to display book information
-    public String toString() {
-        return "Book ID: " + bookID + "\nTitle: " + title + "\nAuthor: " + author + "\nGenre: " + genre + "\nAvailability: " + (availabilityStatus ? "Available" : "Not Available");
+            oos.writeObject(books);
+            oos.writeObject(users);
+
+            oos.close();
+            fos.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
     }
 }
-
